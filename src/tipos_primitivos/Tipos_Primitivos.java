dd6786bf58100cbf5b8ff3de20578f6e1646175f
Mezengas <alexfonsecaramos@hotmail.com>
2022-12-21 11:04:36 -0300

    build nbproject src build manifest.mf

---
+++ src/tipos_primitivos/Tipos_Primitivos.java
@@ -0,0 +1,26 @@
+package tipos_primitivos;
+
+public class Tipos_Primitivos {
+
+    public static void main(String[] args) {
+
+        byte tipoByte = 127;
+        short tipoShort = 32767;
+        char tipoChar = 'A';
+        float tipoFloat = 2.6f;
+        double tipoDouble = 3.59;
+        int tipoInt = 2147483647;
+        long tipoLong = 9223372036854775807l;
+        boolean tipoBooleano = true;
+
+        System.out.println("Tipo do tipoByte : " + tipoByte);
+        System.out.println("Tipo do tipoShort : " + tipoShort);
+        System.out.println("Tipo do tipoChar : " + tipoChar);
+        System.out.println("Tipo do tipoFloat : " + tipoFloat);
+        System.out.println("Tipo do tipoDouble : " + tipoDouble);
+        System.out.println("Tipo do tipoInt : " + tipoInt);
+        System.out.println("Tipo do tipoLong : " + tipoLong);
+        System.out.println("Tipo do tipoBooleano : " + tipoBooleano);
+
+    }
+}
